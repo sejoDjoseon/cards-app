ceaf71fde2b52d3eb54acf297c3c269b8607a805
sejoDjoseon <sergio.morales93@gmail.com>
2023-01-05 19:13:27 +0100

    link component

--- src/app/modules/shared/components/link/link.component.ts
+++ src/app/modules/shared/components/link/link.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-link',
-  templateUrl: './link.component.html',
-  styleUrls: ['./link.component.sass']
+  template: `<u><ng-content></ng-content></u>`,
+  styles: [
+    `
+    :host
+      cursor: pointer
+  `,
+  ],
 })
 export class LinkComponent {
+  @Output() click = new EventEmitter<void>();
 
+  onClick() {
+    console.log('click link');
+  }
 }

d6268531ffd3d5c5a9923f2438e835952d3c5b63
sejoDjoseon <sergio.morales93@gmail.com>
2023-01-08 12:55:29 +0100

    working on list UI

--- src/app/modules/menu/components/lessons-list/components/lessons-list-row/lessons-list-row.component.ts
+++ src/app/modules/menu/components/lessons-list/components/lessons-list-row/lessons-list-row.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-lessons-list-row',
   templateUrl: './lessons-list-row.component.html',
-  styleUrls: ['./lessons-list-row.component.sass']
+  styleUrls: ['./lessons-list-row.component.sass'],
 })
 export class LessonsListRowComponent {
+  showActions = false;
+  @HostListener('mouseenter') onMouseEnter() {
+    this.showActions = true;
+  }
 
+  @HostListener('mouseleave') onMouseLeave() {
+    this.showActions = false;
+  }
 }
--- src/app/modules/menu/components/lessons-list/lessons-list.component.ts
+++ src/app/modules/menu/components/lessons-list/lessons-list.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+const LESSONS_MOCK = [
+  { number: 1, name: 'Ordinales', viewed: true },
+  { number: 1, name: 'Ordinales', viewed: true },
+  { number: 1, name: 'Ordinales', viewed: true },
+  { number: 1, name: 'Ordinales', viewed: true },
+  { number: 1, name: 'Ordinales', viewed: true },
+  { number: 1, name: 'Ordinales', viewed: true },
+  { number: 1, name: 'Ordinales', viewed: true },
+  { number: 1, name: 'Ordinales', viewed: true },
+  { number: 1, name: 'Ordinales', viewed: true },
+  { number: 1, name: 'Ordinales', viewed: true },
+  { number: 1, name: 'Ordinales', viewed: true },
+  { number: 1, name: 'Ordinales', viewed: true },
+  { number: 1, name: 'Ordinales', viewed: true },
+  { number: 1, name: 'Ordinales', viewed: true },
+];
 
 @Component({
   selector: 'app-lessons-list',
   templateUrl: './lessons-list.component.html',
-  styleUrls: ['./lessons-list.component.sass']
+  styleUrls: ['./lessons-list.component.sass'],
 })
 export class LessonsListComponent {
+  faCoffee = faCoffee;
 
+  lessons = LESSONS_MOCK;
 }

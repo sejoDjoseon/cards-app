f11e88898756b0022e03a3a5b593ce6c33e170e8
sejoDjoseon <sergio.morales93@gmail.com>
2023-01-13 11:07:10 +0100

    test WithLoadingPipe

--- src/app/modules/shared/pipes/with-loading.pipe.spec.ts
+++ src/app/modules/shared/pipes/with-loading.pipe.spec.ts
@@ -1,8 +1,37 @@
+import { of, throwError } from 'rxjs';
 import { WithLoadingPipe } from './with-loading.pipe';
+import { subscribeSpyTo } from '@hirez_io/observer-spy';
 
 describe('WithLoadingPipe', () => {
+  const pipe = new WithLoadingPipe();
   it('create an instance', () => {
-    const pipe = new WithLoadingPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('transforms undefined returns undefined', () => {
+    expect(pipe.transform(undefined)).toBeUndefined();
+  });
+
+  it('transforms null returns null', () => {
+    expect(pipe.transform(null)).toBeNull();
+  });
+
+  it('transforms and Observable into an observable starts loading and ends resolved with the value', () => {
+    const result$ = pipe.transform(of(1))!;
+    const observerSpy = subscribeSpyTo(result$);
+    expect(observerSpy.getValues()).toEqual([
+      { loading: true },
+      { loading: false, value: 1 },
+    ]);
+  });
+
+  it('transforms and Observable that fails into an observable starts loading and ends resolved with error', () => {
+    const error = new Error('test');
+    const result$ = pipe.transform(throwError(() => error))!;
+    const observerSpy = subscribeSpyTo(result$);
+    expect(observerSpy.getValues()).toEqual([
+      { loading: true },
+      { loading: false, error: error },
+    ]);
   });
 });
